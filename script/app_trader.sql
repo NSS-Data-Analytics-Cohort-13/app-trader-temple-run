ff01a5bcf04ee644f038042534e8515ffc8aa924
Narendra <kharel.narendra@gmail.com>
2024-10-18 21:35:53 -0400

    Rev3

--- script/app_trader.sql
+++ script/app_trader.sql
@@ -1,75 +1,86 @@
 
 
 	
--- (SELECT * FROM play_store_apps)
--- (SELECT * FROM app_store_apps)
--- ---purchase price calculation
--- WITH app_list as (
--- 	(SELECT name, price, review_count:: int, rating, 'Apple' as store FROM app_store_apps)
+-- -- (SELECT * FROM play_store_apps)
+-- -- (SELECT * FROM app_store_apps)
+-- -- ---purchase price calculation
+-- -- WITH app_list as (
+-- -- 	(SELECT name, price, review_count:: int, rating, 'Apple' as store FROM app_store_apps)
 
--- 	UNION 
+-- -- 	UNION 
 
--- (SELECT name, CAST(TRIM(REPLACE(price, '$', '')) AS numeric(5,2)) AS New_price, review_count, rating, 'Google' as store 
--- 	FROM play_store_apps)
--- 	)
+-- -- (SELECT name, CAST(TRIM(REPLACE(price, '$', '')) AS numeric(5,2)) AS New_price, review_count, rating, 'Google' as store 
+-- -- 	FROM play_store_apps)
+-- -- 	)
  
----create table with required colum and combine both table
+-- ---create table with required colum and combine both table
 
-WITH app_list AS (
-    (SELECT 
-        distinct name, price, review_count::int, rating,'Apple' AS store
- 	FROM app_store_apps)
+-- WITH app_list AS (
+--     (SELECT 
+--         distinct name, price, review_count::int, rating,'Apple' AS store
+--  	FROM app_store_apps)
 
-    UNION 
+--     UNION 
 
-    (SELECT 
-        distinct name, CAST(TRIM(REPLACE(price, '$', '')) AS numeric(5,2)) AS price,review_count::int, rating, 'Google' AS store
+--     (SELECT 
+--         distinct name, CAST(TRIM(REPLACE(price, '$', '')) AS numeric(5,2)) AS price,review_count::int, rating, 'Google' AS store
          
-      FROM play_store_apps)
+--       FROM play_store_apps)
             
-),
-	--SELECT  name, price, review_count, rating, store FROM app_list 
+-- ),
+-- 	--SELECT  name, price, review_count, rating, store FROM app_list 
 
-	---calculate purchase price of apps--
+-- 	life_spans as
+-- 	(
 	
-	 Purchase_price AS ( SELECT name, MAX(price) AS price, 
+-- 	Select name, count(store) as stores_num, (ROUND(AVG(rating) * 2, 0) / 2) as rating,
+-- 		ROUND(1+(2*(ROUND(AVG(rating) * 2, 0) / 2)),2) as life_span
+-- 	from app_list
+-- 	group by name
+-- 	),
+-- 	-- SELECT * from life_spans
+-- 	-- where life_span is not null
+-- 	-- order by life_span desc
+
+-- 	---calculate purchase price of apps--
 	
-	CASE
-			WHEN MAX(price) <= 1 THEN 10000
-			ELSE 10000 * MAX(price)
-		END AS purchase_cost--, review_count, rating, store
-	FROM app_list
-	GROUP BY name
+-- 	 Purchase_price AS ( SELECT name, MAX(price) AS price, 
 	
-	),
---SELECT  name, price, purchase_cost FROM  purchase_price 
-
-revenue as  (
-	select name, price, 5000 as revenue_per_month, store
-	from app_list
-	),
--- select name, price, revenue_per_month, store from revenue
--- order by name
-
-marketing_cost as 
-	(
-select name, 1000*12 as Yearly_marketing_price
-from play_store_apps 
-full join app_store_apps
-using (name)
-	),
--- select name, Yearly_marketing_price
---  from marketing_cost
-
-life_spam as
-	(
+-- 	CASE
+-- 			WHEN MAX(price) <= 1 THEN 10000
+-- 			ELSE 10000 * MAX(price)
+-- 		END AS purchase_cost, review_count, rating, store
+-- 	FROM app_list
+-- 	GROUP BY name, review_count, rating, store
 	
-	Select name, (ROUND((rating) * 2, 0) / 2) as rating,
-		ROUND(1+(2*(ROUND((rating) * 2, 0) / 2)),2) as lifespan
-	from app_list	
-	)
-	SELECT name, 
+-- 	),
+-- --SELECT  name, price, purchase_cost FROM  purchase_price 
 
+-- revenue as  (
+-- 	select p.name, p.purchase_cost, (l.life_span*5000*12) as total_revenue, p.store
+-- 	from purchase_price as p
+-- 	full join life_spans as l
+-- 	--group by name
+-- 	)
+--  select name, purchase_cost, total_revenue, store from revenue
+--  order by name
+
+-- marketing_cost as 
+-- 	(
+-- select name, 1000*12 as Yearly_marketing_price
+-- from play_store_apps 
+-- full join app_store_apps
+-- using (name)
+-- 	),
+-- -- select name, Yearly_marketing_price
+-- --  from marketing_cost
+
+
+
+-- total_cost as (
+
+-- 	Select name, rating, life_span, store, (revenue_per_month*12)*life_span as Total_revenue, (life_span * Yearly_marketing_price)+price as Total_cost,  
+-- )
 	
 	
 
@@ -95,23 +106,42 @@ life_spam as
 -- --LIMIT 10;
 
 
+---TOP 10 LIST of Apps-----
 	
-	-- SELECT name, MAX(price) as price,
- --   COUNT(name)*5000 as profitPerMonth,
- --   COUNT(DISTINCT name)*1000 as costPerMonth,
- --   CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 as decimal(5,2)) as avg_rating
- --   ,CASE WHEN COUNT(name) > 1 THEN 'y' ELSE 'n' END AS availableInBothStores
- --   FROM
- --   (
- --   SELECT name,
- --   CASE WHEN price = '0.00' THEN 10000 ELSE CEILING(price)*10000 END AS price,rating
- --   FROM app_store_apps
- --   UNION
- --  SELECT name,
- --  CASE WHEN CAST(REPLACE(price,'$','') as numeric) = 0 THEN 10000 ELSE
- --  CEILING(CAST(REPLACE(price,'$','') as numeric))*10000 END AS price, rating
- --  FROM play_store_apps)
- --  GROUP BY name;
+	WITH Table_one AS (
+	SELECT name, MAX(price) as price,
+   COUNT(name)*5000 AS Revenue_permonth,
+   COUNT(DISTINCT name)*1000 as cost_PerMonth,
+   CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 AS decimal(5,2)) as Life_yr
+   ,CASE WHEN COUNT(name) > 1 THEN 'y' ELSE 'n' END AS availableInBothStores
+	
+   FROM
+   (
+   SELECT name,
+   CASE WHEN price <= 1 THEN 10000 ELSE CEILING(price)*10000 END AS price,rating
+   FROM app_store_apps
+	
+   UNION
+	
+  SELECT name,
+  CASE WHEN CAST(REPLACE(price,'$','') as numeric) = 0 THEN 10000 ELSE
+  CEILING(CAST(REPLACE(price,'$','') as numeric))*10000 END AS price, rating
+  FROM play_store_apps)
+  GROUP BY name
+),
+
+Table_two AS (
+
+	SELECT name,price, life_yr, (revenue_permonth*12*life_yr) AS Total_revenue, (cost_permonth*12*life_yr) AS Total_cost, ((revenue_permonth*12*life_yr) - (cost_permonth*12*life_yr)) AS profit, availableInBothStores
+
+FROM 	table_one
+	)
+SELECT * FROM Table_two
+	WHERE profit IS NOT NULL
+ORDER BY profit DESC
+limit 10
+
+
          
          
         
