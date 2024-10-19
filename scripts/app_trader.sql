e46ec98ec1153554b1efdbb91846e1e427ab7f27
GooseCorpse21 <zokato18@gmail.com>
2024-10-19 10:35:22 -0400

    need to claen the data up

--- scripts/app_trader.sql
+++ scripts/app_trader.sql
@@ -1,36 +1,83 @@
+SELECT * FROM app_store_apps
+SELECT * FROM play_store_apps
+
+
 -- a. App Trader will purchase apps for 10,000 times the price of the app. For apps that are priced from free up to $1.00, the purchase price is $10,000.
-   SELECT 
-	FROM app_store_apps AS a1
-	INNER JOIN play_store_apps AS p1
 
+SELECT name, MAX(price) as price,
+   COUNT(name)*5000 as profitPerMonth,
+   COUNT(DISTINCT name)*1000 as costPerMonth,
+   CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 as decimal(5,2)) as avg_rating
+   ,CASE WHEN COUNT(name) > 1 THEN 'y' ELSE 'n' END AS availableInBothStores
+   FROM
+   (
+   SELECT name,
+   CASE WHEN price = '0.00' THEN 10000 ELSE CEILING(price)*10000 END AS price,rating
+   FROM app_store_apps
+   UNION
+  SELECT name,
+  CASE WHEN CAST(REPLACE(price,'$','') as numeric) = 0 THEN 10000 ELSE
+  CEILING(CAST(REPLACE(price,'$','') as numeric))*10000 END AS price, rating
+  FROM play_store_apps)
+  GROUP BY name;
+	--SELECT  name, price, review_count, rating, store FROM app_list
+	---calculate purchase price of apps--
+	
+	WITH purchase_price AS ( SELECT name, MAX(price) AS price,
+	
+	CASE
+			WHEN MAX(price) <= 1 THEN 10000
+			ELSE 10000 * MAX(price)
+		END AS purchase_cost--, review_count, rating, store
+	FROM app_list
+	GROUP BY name
+	
+	),
+SELECT  name, price, purchase_cost FROM  purchase_price
+revenue as  (
+	select name, price, 5000 as revenue_per_month, store
+	from app_list
+	),
+-- select name, price, revenue_per_month, store from revenue
+-- order by name
+marketing_cost as
+	(
+select name, 1000*12 as Yearly_marketing_price
+from play_store_apps
+full join app_store_apps
+using (name)
+	),
+-- select name, Yearly_marketing_price
+--  from marketing_cost
+life_spam as
+	(
+	
+	Select name, rating, CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 as decimal(5,2)) as avg_rating
+	from app_list
+	
+	)
 
--- - For example, an app that costs $2.00 will be purchased for $20,000.
-    
--- - The cost of an app is not affected by how many app stores it is on. A $1.00 app on the Apple app store will cost the same as a $1.00 app on both stores. 
-    
--- - If an app is on both stores, it's purchase price will be calculated based off of the highest app price between the two stores. 
+-- #### 3. Deliverables
 
--- b. Apps earn $5000 per month, per app store it is on, from in-app advertising and in-app purchases, regardless of the price of the app.
-    
--- - An app that costs $200,000 will make the same per month as an app that costs $1.00. 
+-- a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
 
--- - An app that is on both app stores will make $10,000 per month. 
-
--- c. App Trader will spend an average of $1000 per month to market an app regardless of the price of the app. If App Trader owns rights to the app in both stores, it can market the app for both stores for a single cost of $1000 per month.
-    
--- - An app that costs $200,000 and an app that costs $1.00 will both cost $1000 a month for marketing, regardless of the number of stores it is in.
-
--- d. For every half point that an app gains in rating, its projected lifespan increases by one year. In other words, an app with a rating of 0 can be expected to be in use for 1 year, an app with a rating of 1.0 can be expected to last 3 years, and an app with a rating of 4.0 can be expected to last 9 years.
-    
--- - App store ratings should be calculated by taking the average of the scores from both app stores and rounding to the nearest 0.5.
-
--- e. App Trader would prefer to work with apps that are available in both the App Store and the Play Store since they can market both for the same $1000 per month.
-
-
-#### 3. Deliverables
-
-a. Develop some general recommendations as to the price range, genre, content rating, or anything else for apps that the company should target.
-
-b. Develop a Top 10 List of the apps that App Trader should buy.
-
-c. Submit a report based on your findings. All analysis work must be done using PostgreSQL, however you may export query results to create charts in Excel 
+-- b. Develop a Top 10 List of the apps that App Trader should buy.
+	SELECT
+	a.name AS app_name
+,	p.name AS play_name
+,	a.rating AS app_rating
+,	p.rating AS play_rating
+,	genres
+,	ROUND(
+        (COALESCE(a.rating, 0) + COALESCE(p.rating, 0)) / --temporarily assume '0' for null rating from a and p rating columns
+        (CASE WHEN a.rating IS NOT NULL AND p.rating IS NOT NULL THEN 2 --divide by 2 if rating in both a and p tables
+              WHEN a.rating IS NOT NULL OR p.rating IS NOT NULL THEN 1 --divide rating by self (1) if a or p rating is null
+              ELSE NULL END), 2 --null result if both a and p rating are null
+    ) AS avg_rating --avg of both app and play ratings considering null values and rounding
+FROM app_store_apps AS a
+	INNER JOIN play_store_apps AS p
+		ON a.name = p.name
+ORDER BY
+	avg_rating DESC
+LIMIT 10;
+-- c. Submit a report based on your findings. All analysis work must be done using PostgreSQL, however you may export query results to create charts in Excel for your report. 
