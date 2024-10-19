4d9c642e36310da276a3da95fb4a5dc5500a8ac4
zaheeruddinlodhi <zaheeruddinlodhi@hotmail.com>
2024-10-19 09:36:37 -0500

    final

--- scripts/Part.d.sql
+++ scripts/Part.d.sql
@@ -83,33 +83,41 @@ WHERE rating IS NOT NULL
 GROUP BY name, rounded_rating
 ORDER BY rounded_rating DESC
 
-SELECT
-	a.name AS app_name
-,	p.name AS play_name
-,	a.rating AS app_rating
-,	p.rating AS play_rating
-,	ROUND(
-        (COALESCE(a.rating, 0) + COALESCE(p.rating, 0)) / --temporarily assume '0' for null rating from a and p rating columns
-        (CASE WHEN a.rating IS NOT NULL AND p.rating IS NOT NULL THEN 2 --divide by 2 if rating in both a and p tables
-              WHEN a.rating IS NOT NULL OR p.rating IS NOT NULL THEN 1 --divide rating by self (1) if a or p rating is null
-              ELSE NULL END), 2 --null result if both a and p rating are null
-    ) AS avg_rating --avg of both app and play ratings considering null values and rounding
-FROM app_store_apps AS a
-	INNER JOIN play_store_apps AS p
-		ON a.name = p.name
-ORDER BY
-	avg_rating DESC
-LIMIT 10;
 
 
+  -- select *
+  -- from app_store_apps
 
-
-
-
-
-  select *
-  from app_store_apps
-
+  
+with Table_one as (
+	SELECT name, MAX(price) as price,
+   COUNT(name)*5000 as Revenue_permonth,
+   COUNT(DISTINCT name)*1000 as cost_PerMonth,
+   CAST((ROUND(AVG(rating)*2.0)/2.0)*2 +1 as decimal(5,2)) as Life_yr
+   ,CASE WHEN COUNT(name) > 1 THEN 'y' ELSE 'n' END AS availableInBothStores
+	
+   FROM
+   (
+   SELECT name,
+   CASE WHEN price <= 1 THEN 10000 ELSE CEILING(price)*10000 END AS price,rating
+   FROM app_store_apps
+	
+   UNION
+	
+  SELECT name,
+  CASE WHEN CAST(REPLACE(price,'$','') as numeric) = 0 THEN 10000 ELSE
+  CEILING(CAST(REPLACE(price,'$','') as numeric))*10000 END AS price, rating
+  FROM play_store_apps)
+  GROUP BY name
+),
+Table_two as (
+	SELECT name,price, life_yr, (revenue_permonth*12*life_yr) as Total_revenue, (cost_permonth*12*life_yr) as Total_cost, ((revenue_permonth*12*life_yr) - (cost_permonth*12*life_yr)) as profit, availableInBothStores
+from 	table_one
+	)
+Select * from Table_two
+	where profit is not null
+order by profit desc
+limit 10
 
 
 
